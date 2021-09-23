7b6dd526543fa9ed8c7bfaa4d7414773470a7943
John Canessa <8019504+JohnCanessa@users.noreply.github.com>
2021-09-23 11:01:41 -0500

    Add files via upload
    First Java code commit

---
+++ MinimumLoss.java
@@ -0,0 +1,101 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+
+
+/**
+ * 
+ */
+public class MinimumLoss {
+
+
+    /**
+     * Buy the house in one year and sell it in another, 
+     * and must be done at a loss.
+     * One must minimize the financial loss.
+     * Time: O (n log(n)) - Space: O(n)
+     */
+    public static int minimumLoss(List<Long> prices) {
+
+        // **** sanity check(s) ****
+        if (prices.size() < 2) return 0;
+
+        // **** initialization ****
+        long minLoss = Long.MAX_VALUE;
+        long dayLoss = 0;
+
+        // **** allocate and populate map for prices and indices - O(n) ****
+        Map<Long, Integer> priceIndexMap = new HashMap<>();
+        for (int i = 0; i < prices.size(); i++)
+            priceIndexMap.put(prices.get(i), i);
+
+        // ???? ????
+        System.out.println("minimumLoss <<< priceIndexMap: " + priceIndexMap.toString());
+
+        // **** sort the price list - O(n log(n)) ****
+        Collections.sort(prices);
+
+        // ???? ????
+        System.out.println("minimumLoss <<< prices: " + prices.toString());
+
+        // **** traverse the sorted list of prices from
+        //       high to low looking for the minimum loss - O(n) ****
+        for (int i = prices.size() - 1; i > 0; i--) {
+           
+            // **** skip indices out of order (need to BUY before SELL) ****
+            if (priceIndexMap.get(prices.get(i)) > priceIndexMap.get(prices.get(i - 1)))
+                continue;
+
+            // **** compute day loss ****
+            dayLoss = prices.get(i) - prices.get(i - 1);
+
+            // ???? ????
+            System.out.println("minimumLoss <<< dayLoss: " + dayLoss);
+
+            // **** update the minimum loss ****
+            minLoss = (dayLoss < minLoss) ? dayLoss : minLoss;
+
+            // ???? ????
+            System.out.println("minimumLoss <<< minLoss: " + minLoss);
+        }
+
+        // **** return the minimum loss ****
+        return (int)minLoss;
+    }
+
+
+    /**
+     * Test scaffold.
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException {
+        
+        // **** open buffered reader ****
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // **** skip count of prices ****
+        br.readLine();
+
+        // **** read price list into a list ****
+        List<Long> prices = Stream.of(br.readLine().trim().split(" "))
+                                .map(Long::parseLong)
+                                .collect(Collectors.toList());
+
+        // **** close buffered reader ****
+        br.close();
+
+        // ???? ????
+        System.out.println("main <<< prices: " + prices.toString());
+
+        // **** ****
+        System.out.println("main <<< minimumLoss: " + minimumLoss(prices));
+    }
+
+}
